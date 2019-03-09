03612e9f103e22e2c9e0613c22ed4d8627077973
justinan <antonio.justino@arhs-cube.com>
2019-03-09 14:16:44 +0100

    thumnails

--- src/main/java/com/greglturnquist/payroll/data/SubjectTO.java
+++ src/main/java/com/greglturnquist/payroll/data/SubjectTO.java
@@ -4,9 +4,26 @@ import com.greglturnquist.payroll.repository.DocumentUrl;
 import com.greglturnquist.payroll.repository.Subject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SubjectTO {
+
+    private static Map<Long, String> colorMap = new HashMap<>();
+
+    private static final Map<Long, String> thumbnailMap = new HashMap();
+
+    static {
+        thumbnailMap.put(1L, "thumbnails/1877-01-07_01-00001.jpg");
+        thumbnailMap.put(2L, "thumbnails/1877-01-09_01-00001.jpg");
+        thumbnailMap.put(3l, "thumbnails/1877-01-13_01-00001.jpg");
+        thumbnailMap.put(4L, "thumbnails/1877-02-03_01-00001.jpg");
+        thumbnailMap.put(5L, "thumbnails/1877-02-10_01-00001.jpg");
+        thumbnailMap.put(6L, "thumbnails/1877-04-01_01-00001.jpg");
+        thumbnailMap.put(7L, "thumbnails/1877-09-30_01-00001.jpg");
+        thumbnailMap.put(8L, "thumbnails/1877-10-20_01-00001.jpg");
+    }
 
     private Long id;
     private String title;
@@ -17,7 +34,7 @@ public class SubjectTO {
     public SubjectTO(Subject subject) {
         this.id = subject.getId();
         this.title = subject.getName();
-        this.thumbnail = "";
+        this.thumbnail = thumbnailMap.get(subject.getId()%8);
         this.url = new ArrayList<>();
         for (DocumentUrl doc : subject.getDocuments()) {
             getUrls().add(doc.getUrl());
