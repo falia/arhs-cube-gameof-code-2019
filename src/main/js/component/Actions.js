e2367da491ebba56432fe2e2235161b7df89a252
Faton Alia <Faton.Alia@arhs-cube.com>
2019-03-09 01:08:14 +0100

    update modal

--- src/main/js/component/Actions.js
+++ src/main/js/component/Actions.js
@@ -1,17 +1,6 @@
 import React from "react";
 import PopPop from 'react-poppop';
 
-const customStyles = {
-    content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
-    }
-};
-
 class Actions extends React.Component {
 
     render() {
@@ -21,9 +10,7 @@ class Actions extends React.Component {
                 <PopPop position="centerCenter"
                         open={this.props.show}
                         closeBtn={true}
-                        closeOnEsc={true}
                         onClose={() => this.props.handleOnClick(false)}
-                        closeOnOverlay={true}
                         className="Modal"
                         overlayClassName="Overlay">
                 </PopPop>
